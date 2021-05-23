3e86f9ce623eea6411eff43a208e08bfd7a827a6
emensah77 <40795962+emensah77@users.noreply.github.com>
2021-05-23 15:29:45 -0700

    Top tab bar navigator added.

--- src/navigation/ExploreNavigator.js
+++ src/navigation/ExploreNavigator.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../screens/Home';
 import SearchResultsScreen from '../screens/SearchResults';
-
+import SearchResultsTabNavigator from './SearchResultsTabNavigator';
 const Stack = createStackNavigator();
 
 const Router = (props) => {
@@ -21,7 +21,7 @@ const Router = (props) => {
 
                     <Stack.Screen
                         name={'SearchResults'}
-                        component={SearchResultsScreen}
+                        component={SearchResultsTabNavigator}
                         options={{
                            title: 'Search your destination',
                         }}
---
+++ src/navigation/SearchResultsTabNavigator.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {View, Text} from 'react-native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import SearchResults from '../screens/SearchResults';
+const Tab = createMaterialTopTabNavigator();
+
+const SearchResultsTabNavigator = (props) => {
+    return (
+        <Tab.Navigator tabBarOptions={{
+                activeTintColor: 'cyan',
+                indicatorStyle: {
+                    backgroundColor: 'cyan'
+                }
+        }}>
+            <Tab.Screen 
+                name={"list"}
+                component={SearchResults} />
+
+            <Tab.Screen 
+                name={"map"}
+                component={SearchResults} />
+        </Tab.Navigator>
+    );
+};
+
+export default SearchResultsTabNavigator;
