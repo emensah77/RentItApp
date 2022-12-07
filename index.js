b8167f7b11924c0755e1bf5e94f6ca066705a5ea
dhaivat khanpara <dhaivat.khanpara@gmail.com>
2022-12-08 00:39:43 +0530

    Fix build error ios

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import config from './src/aws-exports'
 import Reactotron from 'reactotron-react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 Amplify.configure(config)
-
+import 'react-native-gesture-handler';
 
 Reactotron
   .setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
