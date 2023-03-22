156f86410f278382bcbc9a255d7737f213967f2d
Bilal Mehmoood <bmehmood199@gmail.com>
2023-03-22 10:29:08 +0500

    added missing query

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -438,3 +438,58 @@ export const listPostNews = /* GraphQL */ `
     }
   }
 `;
+
+export const getUserHomes = /* GraphQL */ `
+  query getUser($id: ID!) {
+    getUser(id: $id) {
+      id
+      email
+      createdAt
+      posts(limit: 10000000) {
+        items {
+          id
+          image
+          images
+          type
+          title
+          description
+          mode
+          phoneNumbers
+          bed
+          bedroom
+          bathroomNumber
+          maxGuests
+          wifi
+          kitchen
+          bathroom
+          water
+          toilet
+          aircondition
+          locality
+          sublocality
+          status
+          oldPrice
+          newPrice
+          latitude
+          longitude
+          createdAt
+          updatedAt
+          userID
+          reviews(sortDirection: DESC, limit: 25) {
+            items {
+              createdAt
+              id
+              postId
+              rating
+              review
+              updatedAt
+              userID
+            }
+          }
+        }
+      }
+      username
+      imageuri
+    }
+  }
+`;
