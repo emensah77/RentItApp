0a0f33a076b8068211719d33084b0cf6ebc5bf58
Bilal Mehmoood <bmehmood199@gmail.com>
2023-03-27 13:16:35 +0500

    Added reviews of a home

--- src/screens/Reviews/ReviewCard/CardCommentPhoto.js
+++ src/screens/Reviews/ReviewCard/CardCommentPhoto.js
@@ -1,8 +1,9 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import {Text} from 'react-native';
-import {Image, View} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text } from 'react-native';
+import { Image, View } from 'react-native';
 import StarRating from '../../../components/StarRating';
+import firebase from '@react-native-firebase/app';
 import styles from './styles';
 
 export default function CardCommentPhoto({
@@ -13,36 +14,56 @@ export default function CardCommentPhoto({
   title,
   comment,
   image,
+  userID,
+  hasReply
 }) {
+
+  const [userInfo, setUserInfo] = useState()
+
+  const getUserInfo = async (id) => {
+    if (id) {
+      try {
+        const data = await firebase.firestore().collection('users').doc(id).get();
+        setUserInfo(data.data())
+        return data.data();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+  useEffect(() => {
+    getUserInfo(userID)
+  }, [])
+
   return (
     <View style={[styles.contain, style]}>
-      <View style={{flexDirection: 'row', marginBottom: 10}}>
+      <View style={{ flexDirection: 'row', marginBottom: 10 }}>
         <Image
-          source={{uri: image, width: 200, height: 200}}
+          source={{ uri: userInfo?.userImg || 'https://w7.pngwing.com/pngs/754/2/png-transparent-samsung-galaxy-a8-a8-user-login-telephone-avatar-pawn-blue-angle-sphere-thumbnail.png', width: 200, height: 200 }}
           style={styles.thumb}
         />
         <View
           style={{
             flex: 1,
           }}>
-          <Text headline numberOfLines={1}>
-            {name}
+          <Text style={{ fontWeight: '600', marginLeft: 3 }} numberOfLines={1}>
+            {userInfo?.displayName || userInfo?.fname}
           </Text>
 
           <View style={styles.contentRate}>
-            <StarRating
+            {!hasReply && <StarRating
               disabled={true}
               starSize={14}
               maxStars={5}
               rating={rate}
-              selectedStar={rating => {}}
+              selectedStar={rating => { }}
               fullStarColor={'orange'}
-            />
+            />}
             <Text
               footnote
               grayColor
               numberOfLines={1}
-              style={{paddingHorizontal: 4}}>
+              style={{ paddingHorizontal: 4, color: 'gray' }}>
               {date}
             </Text>
           </View>
@@ -52,6 +73,7 @@ export default function CardCommentPhoto({
         <Text
           style={{
             marginTop: 10,
+            marginLeft: 10
           }}>
           {comment}
         </Text>
@@ -69,8 +91,8 @@ CardCommentPhoto.defaultProps = {
   title: '',
   comment: '',
   images: [],
-  onYes: () => {},
-  onNo: () => {},
+  onYes: () => { },
+  onNo: () => { },
   totalLike: 0,
-  openGallery: () => {},
+  openGallery: () => { },
 };
