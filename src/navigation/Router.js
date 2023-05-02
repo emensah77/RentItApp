7a59950a28c298f180452856e0e0ee04810c2526
Abdumutal Abdusamatov <aka.orihero@gmail.com>
2023-05-02 15:22:46 +0500

    refactoring

--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -1,7 +1,6 @@
 import React, {useContext, useState, useEffect, useRef, useCallback} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import auth from '@react-native-firebase/auth';
-import {ActivityIndicator} from 'react-native';
 import analytics from '@react-native-firebase/analytics';
 
 import AuthStack from './AuthStack';
