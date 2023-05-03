9b278fbbcdc347d44e5eb41d3f71e9c30d5febbf
Abdumutal Abdusamatov <aka.orihero@gmail.com>
2023-05-03 14:54:10 +0500

    refactoring

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,14 +1,13 @@
 import {Dimensions, Platform} from 'react-native';
 
 // window sizes
-export const SIZES = Dimensions.get('window');
+export const windowSize = () => Dimensions.get('window');
 
 // platform
 export const isIOS = Platform.OS === 'ios';
 export const isAndroid = Platform.OS === 'android';
 
 export const CONSTANTS = {
-  SIZES,
   isAndroid,
   isIOS,
 };
