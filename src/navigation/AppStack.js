9654ef8748ea78fbdbaad345d22a51382486c9be
Chijioke Nna <nnachijioke@yahoo.com>
2023-05-19 10:16:39 +0100

    Patch AppStack

--- src/navigation/AppStack.js
+++ src/navigation/AppStack.js
@@ -1,68 +1,70 @@
-import React, {useMemo, useContext, lazy, Suspense} from 'react';
+import React, {useMemo, useCallback} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import auth from '@react-native-firebase/auth';
 
-import {AuthContext} from './AuthProvider';
 import mixpanel from '../MixpanelConfig';
 
-import PageSpinner from '../components/PageSpinner';
-
-const DestinationSearchScreen = lazy(() => import('../screens/DestinationSearch'));
-const GuestsScreen = lazy(() => import('../screens/GuestsScreen'));
-const PostScreen = lazy(() => import('../screens/PostScreen'));
-const HouseTypeScreen = lazy(() => import('../screens/HouseTypeScreen'));
-const WelcomeScreen = lazy(() => import('../screens/WelcomeScreen'));
-const EFeedback = lazy(() => import('../screens/Feedback'));
-const Review = lazy(() => import('../screens/Reviews'));
-const HomeTabNavigator = lazy(() => import('./HomeTabNavigator'));
+import DestinationSearchScreen from '../screens/DestinationSearch';
+import GuestsScreen from '../screens/GuestsScreen';
+import PostScreen from '../screens/PostScreen';
+import HouseTypeScreen from '../screens/HouseTypeScreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
+import EFeedback from '../screens/Feedback';
+import Review from '../screens/Reviews';
+import HomeTabNavigator from './HomeTabNavigator';
 
 const Stack = createStackNavigator();
 
-const onNavigationStateChange = user => state => {
-  const currentRoute = state.routes[state.index];
-  const currentScreen = currentRoute.name;
-
-  mixpanel.track('Screen Viewed', {
-    screenName: currentScreen,
-    userId: user ? user.uid : 'guest',
-  });
-};
-
 const AppStack = () => {
-  const {user} = useContext(AuthContext);
-
   const noHeader = useMemo(
     () => ({
       headerShown: false,
+      lazy: true,
     }),
     [],
   );
 
+  const noHeaderWithATitle = useMemo(
+    () => ({
+      title: 'Search your destination',
+      headerShown: false,
+      lazy: true,
+    }),
+    [],
+  );
+
+  const onNavigationStateChange = useCallback(state => {
+    const currentRoute = state.routes[state.index];
+    const screenName = currentRoute.name;
+    const user = auth().currentUser;
+    mixpanel.track('Screen Viewed', {
+      screenName,
+      userId: user ? user.uid : 'guest',
+    });
+  }, []);
+
   return (
-    <Suspense fallback={<PageSpinner />}>
-      <Stack.Navigator onStateChange={onNavigationStateChange(user)}>
-        <Stack.Screen name="Home" component={HomeTabNavigator} options={noHeader} />
+    <Stack.Navigator onStateChange={onNavigationStateChange}>
+      <Stack.Screen name="Home" component={HomeTabNavigator} options={noHeader} />
 
-        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={noHeader} />
+      <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={noHeader} />
 
-        <Stack.Screen
-          name="Destination Search"
-          component={DestinationSearchScreen}
-          options={{
-            title: 'Search your destination',
-            headerShown: false,
-          }}
-        />
+      <Stack.Screen
+        name="Destination Search"
+        component={DestinationSearchScreen}
+        options={noHeaderWithATitle}
+      />
 
-        <Stack.Screen name="Number of Guests" component={GuestsScreen} options={noHeader} />
-        <Stack.Screen name="House Type" component={HouseTypeScreen} options={noHeader} />
+      <Stack.Screen name="Number of Guests" component={GuestsScreen} options={noHeader} />
 
-        <Stack.Screen name="Post" component={PostScreen} options={noHeader} />
+      <Stack.Screen name="House Type" component={HouseTypeScreen} options={noHeader} />
 
-        <Stack.Screen name="Reviews" component={Review} options={noHeader} />
+      <Stack.Screen name="Post" component={PostScreen} options={noHeader} />
 
-        <Stack.Screen name="Feedback" component={EFeedback} options={noHeader} />
-      </Stack.Navigator>
-    </Suspense>
+      <Stack.Screen name="Reviews" component={Review} options={noHeader} />
+
+      <Stack.Screen name="Feedback" component={EFeedback} options={noHeader} />
+    </Stack.Navigator>
   );
 };
 
