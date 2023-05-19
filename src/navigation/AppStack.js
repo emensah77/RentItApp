350f231b07ed76f271d6b5d45f35e410b4e85c74
Nna Chijioke E <nnachijioke@yahoo.com>
2023-05-19 10:07:45 +0100

    Readd AppStack changes

--- src/navigation/AppStack.js
+++ src/navigation/AppStack.js
@@ -1,17 +1,19 @@
-import React, {useMemo, useContext} from 'react';
+import React, {useMemo, useContext, lazy, Suspense} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import {AuthContext} from './AuthProvider';
 import mixpanel from '../MixpanelConfig';
 
-import DestinationSearchScreen from '../screens/DestinationSearch';
-import GuestsScreen from '../screens/GuestsScreen';
-import PostScreen from '../screens/PostScreen';
-import HouseTypeScreen from '../screens/HouseTypeScreen';
-import WelcomeScreen from '../screens/WelcomeScreen';
-import EFeedback from '../screens/Feedback';
-import Review from '../screens/Reviews';
-import HomeTabNavigator from './HomeTabNavigator';
+import PageSpinner from '../components/PageSpinner';
+
+const DestinationSearchScreen = lazy(() => import('../screens/DestinationSearch'));
+const GuestsScreen = lazy(() => import('../screens/GuestsScreen'));
+const PostScreen = lazy(() => import('../screens/PostScreen'));
+const HouseTypeScreen = lazy(() => import('../screens/HouseTypeScreen'));
+const WelcomeScreen = lazy(() => import('../screens/WelcomeScreen'));
+const EFeedback = lazy(() => import('../screens/Feedback'));
+const Review = lazy(() => import('../screens/Reviews'));
+const HomeTabNavigator = lazy(() => import('./HomeTabNavigator'));
 
 const Stack = createStackNavigator();
 
@@ -31,42 +33,36 @@ const AppStack = () => {
   const noHeader = useMemo(
     () => ({
       headerShown: false,
-      lazy: true,
-    }),
-    [],
-  );
-
-  const noHeaderWithATitle = useMemo(
-    () => ({
-      title: 'Search your destination',
-      headerShown: false,
-      lazy: true,
     }),
     [],
   );
 
   return (
-    <Stack.Navigator onStateChange={onNavigationStateChange(user)}>
-      <Stack.Screen name="Home" component={HomeTabNavigator} options={noHeader} />
+    <Suspense fallback={<PageSpinner />}>
+      <Stack.Navigator onStateChange={onNavigationStateChange(user)}>
+        <Stack.Screen name="Home" component={HomeTabNavigator} options={noHeader} />
 
-      <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={noHeader} />
+        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={noHeader} />
 
-      <Stack.Screen
-        name="Destination Search"
-        component={DestinationSearchScreen}
-        options={noHeaderWithATitle}
-      />
+        <Stack.Screen
+          name="Destination Search"
+          component={DestinationSearchScreen}
+          options={{
+            title: 'Search your destination',
+            headerShown: false,
+          }}
+        />
 
-      <Stack.Screen name="Number of Guests" component={GuestsScreen} options={noHeader} />
+        <Stack.Screen name="Number of Guests" component={GuestsScreen} options={noHeader} />
+        <Stack.Screen name="House Type" component={HouseTypeScreen} options={noHeader} />
 
-      <Stack.Screen name="House Type" component={HouseTypeScreen} options={noHeader} />
+        <Stack.Screen name="Post" component={PostScreen} options={noHeader} />
 
-      <Stack.Screen name="Post" component={PostScreen} options={noHeader} />
+        <Stack.Screen name="Reviews" component={Review} options={noHeader} />
 
-      <Stack.Screen name="Reviews" component={Review} options={noHeader} />
-
-      <Stack.Screen name="Feedback" component={EFeedback} options={noHeader} />
-    </Stack.Navigator>
+        <Stack.Screen name="Feedback" component={EFeedback} options={noHeader} />
+      </Stack.Navigator>
+    </Suspense>
   );
 };
 
