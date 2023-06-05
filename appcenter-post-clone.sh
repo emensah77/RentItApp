ffc8970b4fb4fa47157efe4aa34485fee10dc06d
Chijioke Nna <nnachijioke@yahoo.com>
2023-06-05 17:38:44 +0100

    Adds app-center config

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 cat << EOF > $APPCENTER_SOURCE_DIRECTORY/android/app/src/main/assets/appcenter-config.json
-  NEXT_PUBLIC_NODE_ENV=${{ APPCENTER_CONFIG }}
+  ${{ APPCENTER_CONFIG }}
 EOF
+
+cd $APPCENTER_SOURCE_DIRECTORY/android/app/src/main/assets/appcenter-config.json
+ls -lSA 
