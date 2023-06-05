3a29f204942edb6d3ca1d8e91091ffc1a2b0e712
Chijioke Nna <nnachijioke@yahoo.com>
2023-06-05 18:02:15 +0100

    Adds app-center config

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -4,5 +4,4 @@ cat << EOF > $APPCENTER_SOURCE_DIRECTORY/android/app/src/main/assets/appcenter-c
   $APPCENTER_CONFIG
 EOF
 
-cd $APPCENTER_SOURCE_DIRECTORY/android/app/src/main/assets
-ls -lSA 
+cat $APPCENTER_SOURCE_DIRECTORY/android/app/src/main/assets
