fd8d80dda1d4ad7e67aaa68c13ee6597cf4e3c18
Chijioke Nna <nnachijioke@yahoo.com>
2023-06-21 10:17:04 +0100

    Add coverage to tests

--- jest.config.js
+++ jest.config.js
@@ -8,24 +8,23 @@ module.exports = {
   testTimeout: 500000,
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest',
-    // '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   globals: {
     __DEV__: true,
   },
-  // collectCoverage: true,
-  // coverageDirectory: 'analysis/coverage',
-  // coveragePathIgnorePatterns: ['/node_modules/'],
-  // collectCoverageFrom: ['src/**/*.{js,jsx,ts}'],
-  // coverageReporters: ['json', 'lcov', 'text', 'clover'],
-  // coverageThreshold: {
-  //   global: {
-  //     branches: 0,
-  //     functions: 0,
-  //     lines: 0,
-  //     statements: 0,
-  //   },
-  // },
+  collectCoverage: true,
+  coverageDirectory: 'analysis/coverage',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts}'],
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
   clearMocks: true,
   coverageProvider: 'babel',
   testPathIgnorePatterns: ['\\\\node_modules\\\\', '__tests__/helper.js'],
