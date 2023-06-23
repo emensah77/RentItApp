0e649eca84672cbb7be775b24a1d21c86780a149
Nna Chijioke E <nnachijioke@yahoo.com>
2023-06-23 21:54:48 +0100

    Update appcenter-post-build.sh

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -4,7 +4,7 @@
 
 appcenter login --password $APPCENTER_PASSWORD --user $APPCENTER_USER --quiet --version
 
-appcenter test generate appium --platform $Platform --output-path $APPCENTER_SOURCE_DIRECTORY/chijioke
+appcenter test generate appium --platform $PLATFORM --output-path $APPCENTER_SOURCE_DIRECTORY/chijioke
 
 SOURCE_APK=$APPCENTER_SOURCE_DIRECTORY/android/app/build/outputs/apk/release/app-release.apk
 
@@ -14,11 +14,11 @@ npm install -g replace-in-file
 
 cat $APPCENTER_SOURCE_DIRECTORY/chijioke/src/test/java/com/azure/mobile/app/test/LaunchTest.java
 
-replace-in-file "capabilities.setCapability(\"app\", \"/path/to/app.apk\");" "capabilities.setCapability(\"app\", \"$SOURCE_APK\");" $APPCENTER_SOURCE_DIRECTORY/chijioke/src/test/java/com/azure/mobile/app/test/LaunchTest.java --encoding=utf-8
+replace-in-file "capabilities.setCapability(\"app\", \"/path/to/app.apk\");" "capabilities.setCapability(\"app\", \"$SOURCE_APK\");" $APPCENTER_SOURCE_DIRECTORY/chijioke/src/test/java/com/azure/mobile/app/test/LaunchTest.java --encoding=utf-8 --disableGlobs true
 
-mkdir /path
-mkdir /path/to
-cp $SOURCE_APK /path/to/app.apk
+# mkdir /path
+# mkdir /path/to
+# cp $SOURCE_APK /path/to/app.apk
 cd $APPCENTER_SOURCE_DIRECTORY/chijioke
 
 appcenter test run appium --app "Photizo/Rentit-Android-Production" --devices "Photizo/$DEVICE_SET" --app-path $APPCENTER_SOURCE_DIRECTORY/android/app/build/outputs/apk/release/app-release.apk --test-series "launch-tests" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/chijioke/target/upload
