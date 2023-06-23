d00366adac845a94e6e68fb58b0b55e7a483bf2a
Nna Chijioke E <nnachijioke@yahoo.com>
2023-06-23 20:36:37 +0100

    Update appcenter-post-build.sh

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-APPCENTER_BRANCH="dev"
-APPCENTER_SOURCE_DIRECTORY="/c/Apps/Slash.Dev/RentItApp"
-Platform="android"
 [[ $APPCENTER_BRANCH = "staging" || $APPCENTER_BRANCH = "main" ]] && DEVICE_SET="release" || DEVICE_SET="development"
 
 appcenter login --password $APPCENTER_PASSWORD --user $APPCENTER_USER --quiet --version
@@ -19,9 +16,9 @@ cat $APPCENTER_SOURCE_DIRECTORY/chijioke/src/test/java/com/azure/mobile/app/test
 
 replace-in-file "capabilities.setCapability(\"app\", \"/path/to/app.apk\");" "capabilities.setCapability(\"app\", \"$SOURCE_APK\");" $APPCENTER_SOURCE_DIRECTORY/chijioke/src/test/java/com/azure/mobile/app/test/LaunchTest.java --encoding=utf-8
 
-cd $APPCENTER_SOURCE_DIRECTORY/chijioke
 mkdir /path
 mkdir /path/to
 cp $SOURCE_APK /path/to/app.apk
+cd $APPCENTER_SOURCE_DIRECTORY/chijioke
 
 appcenter test run appium --app "Photizo/Rentit-Android-Production" --devices "Photizo/$DEVICE_SET" --app-path $APPCENTER_SOURCE_DIRECTORY/android/app/build/outputs/apk/release/app-release.apk --test-series "launch-tests" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/chijioke/target/upload
