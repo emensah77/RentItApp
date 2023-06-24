290bcd6af873f6435ac0f8799d5ba6e971305b65
Nna Chijioke E <nnachijioke@yahoo.com>
2023-06-24 07:14:58 +0100

    Update appcenter-post-build.sh

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -2,23 +2,22 @@
 
 [[ $APPCENTER_BRANCH = "staging" || $APPCENTER_BRANCH = "main" ]] && DEVICE_SET="release" || DEVICE_SET="development"
 
-appcenter login --password $APPCENTER_PASSWORD --user $APPCENTER_USER --quiet --version
+echo "Logging In\n"
+appcenter login --password $APPCENTER_PASSWORD --user $APPCENTER_USER --quiet --version --debug
+echo "Logged In\n"
 
-appcenter test generate appium --platform $PLATFORM --output-path $APPCENTER_SOURCE_DIRECTORY/chijioke
+echo "Generating Launch Test\n"
+appcenter test generate appium --platform $PLATFORM --output-path $APPCENTER_SOURCE_DIRECTORY/chijioke --debug
+echo "Generated Launch Test\n"
 
 SOURCE_APK=$APPCENTER_SOURCE_DIRECTORY/android/app/build/outputs/apk/release/app-release.apk
 
-echo $SOURCE_APK;
-
 npm install -g replace-in-file
-
-cat $APPCENTER_SOURCE_DIRECTORY/chijioke/src/test/java/com/azure/mobile/app/test/LaunchTest.java
 
 replace-in-file "capabilities.setCapability(\"app\", \"/path/to/app.apk\");" "capabilities.setCapability(\"app\", \"$SOURCE_APK\");" $APPCENTER_SOURCE_DIRECTORY/chijioke/src/test/java/com/azure/mobile/app/test/LaunchTest.java --encoding=utf-8 --disableGlobs true
 
-# mkdir /path
-# mkdir /path/to
-# cp $SOURCE_APK /path/to/app.apk
 cd $APPCENTER_SOURCE_DIRECTORY/chijioke
 
-appcenter test run appium --app "Photizo/Rentit-Android-Production" --devices "Photizo/$DEVICE_SET" --app-path $APPCENTER_SOURCE_DIRECTORY/android/app/build/outputs/apk/release/app-release.apk --test-series "launch-tests" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/chijioke/target/upload
+echo "Starting Run\n"
+appcenter test run appium --app "Photizo/Rentit-Android-Production" --devices "Photizo/$DEVICE_SET" --app-path $APPCENTER_SOURCE_DIRECTORY/android/app/build/outputs/apk/release/app-release.apk --test-series "launch-tests" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/chijioke/target/upload --token $APPCENTER_CLI_TOKEN --debug
+echo "Completed Run"
