cbab180c71b60924e1fe1ab23e5f388f8c2c148d
Chijioke Nna <nnachijioke@yahoo.com>
2023-07-04 14:22:07 +0100

    Attempt to use symlinks to fix require issues

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-ln -sf $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/bin/sentry-cli $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/sentry-cli
+# ln -sf $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/bin/sentry-cli $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/sentry-cli
+
+cp -a $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/. $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/
+cp -a $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/bin/sentry-cli $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/sentry-cli
