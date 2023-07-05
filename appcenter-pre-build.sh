855c0cfb1ab9230cfc7d11fc526fe39966e1b42f
Chijioke Nna <nnachijioke@yahoo.com>
2023-07-05 07:08:45 +0100

    Attempt installing sentry post install

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,10 +2,12 @@
 
 # ln -sf $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/bin/sentry-cli $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/sentry-cli
 
-ls -lSA $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli
+ls -lSA $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry
+echo "\n\n\n\n\n\n"
 
 # node $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/scripts/install.js
 cp -a $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/. $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/
 cp -a $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/bin/sentry-cli $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/sentry-cli
 
-ls -lSA $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli
+echo "\n\n\n\n\n\n"
+ls -lSA $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry
