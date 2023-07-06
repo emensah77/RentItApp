ad0322a90837539c3aee1398ebbcb3a1ec1aea0d
Chijioke Nna <nnachijioke@yahoo.com>
2023-07-06 14:53:02 +0100

    Adds code push

--- android/app/src/main/java/com/airbnbclone/MainApplication.java
+++ android/app/src/main/java/com/airbnbclone/MainApplication.java
@@ -14,6 +14,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
+import com.microsoft.codepush.react.CodePush;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -38,6 +39,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected String getJSBundleFile() {
+          return CodePush.getJSBundleFile();
         }
       };
 
