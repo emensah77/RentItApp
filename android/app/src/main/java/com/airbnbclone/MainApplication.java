a1934f6b3275e808ea264fd5c4551cfa2ba55e43
Chijioke Nna <nnachijioke@yahoo.com>
2023-07-07 17:59:34 +0100

    Patch build

--- android/app/src/main/java/com/airbnbclone/MainApplication.java
+++ android/app/src/main/java/com/airbnbclone/MainApplication.java
@@ -15,6 +15,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
 import com.microsoft.codepush.react.CodePush;
+import com.lugg.RNCConfig.RNCConfigPackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
