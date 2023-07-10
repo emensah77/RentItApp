ea21ffbd414de96d085fe7acd3cbef035f3153c6
Nna Chijioke E <nnachijioke@yahoo.com>
2023-07-10 20:35:37 +0100

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -5,7 +5,7 @@
 # ls -lSA $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry
 # echo "\n\n\n\n\n\n"
 
-node $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/scripts/install.js
+# node $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/scripts/install.js
 # cp -af $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/. $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/
 # cp -f $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/bin/sentry-cli $APPCENTER_SOURCE_DIRECTORY/node_modules/@sentry/cli/sentry-cli
 
