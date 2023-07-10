b485f7daa1ccfd492ac8f58efb34350d35cc26d5
Chijioke Nna <nnachijioke@yahoo.com>
2023-07-10 16:24:17 +0100

    Performance improvements

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,16 @@ module.exports = {
     'plugin:import/warnings',
     '@react-native-community',
   ],
-  plugins: ['react', 'prettier', 'sort-keys-fix', 'jsx-a11y', '@babel', 'react-hooks'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'react',
+    'prettier',
+    'sort-keys-fix',
+    'jsx-a11y',
+    '@babel',
+    'react-hooks',
+    '@typescript-eslint',
+  ],
   rules: {
     indent: [
       'error',
@@ -56,7 +65,6 @@ module.exports = {
     'import/no-unresolved': [0],
     'import/extensions': [0],
     'import/prefer-default-export': [0],
-    'global-require': [0],
     'react/require-default-props': [0],
     'react/destructuring-assignment': [0],
   },
