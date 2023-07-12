9b13a1bcfec7fe9bd43b11bd5ea938654604fda3
Chijioke Nna <nnachijioke@yahoo.com>
2023-07-12 10:36:51 +0100

    Add build commands to git actions

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+echo "$NODE_BINARY" $NODE_ARGS "$CLI_PATH" $BUNDLE_COMMAND \
+  $CONFIG_ARG \
+  --entry-file "$ENTRY_FILE" \
+  --platform "$BUNDLE_PLATFORM" \
+  --dev $DEV \
+  --reset-cache \
+  --bundle-output "$BUNDLE_FILE" \
+  --assets-dest "$DEST" \
+  $EXTRA_ARGS \
+  $EXTRA_PACKAGER_ARGS
+
 # Create Android App Center config json file
 cat << EOF > $APPCENTER_SOURCE_DIRECTORY/android/app/src/main/assets/appcenter-config.json
   $APPCENTER_CONFIG
@@ -21,14 +32,3 @@ cat << EOF > $APPCENTER_SOURCE_DIRECTORY/.env
 REACT_NATIVE_APP_ENV=$REACT_NATIVE_APP_ENV
 CODE_PUSH_DEPLOYMENT_KEY=$CODE_PUSH_DEPLOYMENT_KEY
 EOF
-
-echo "$NODE_BINARY" $NODE_ARGS "$CLI_PATH" $BUNDLE_COMMAND \
-  $CONFIG_ARG \
-  --entry-file "$ENTRY_FILE" \
-  --platform "$BUNDLE_PLATFORM" \
-  --dev $DEV \
-  --reset-cache \
-  --bundle-output "$BUNDLE_FILE" \
-  --assets-dest "$DEST" \
-  $EXTRA_ARGS \
-  $EXTRA_PACKAGER_ARGS
