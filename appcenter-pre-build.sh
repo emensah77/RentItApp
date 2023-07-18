4243543ea02bd42439ff77f1cecb54f7eb2690e1
Chijioke Nna <nnachijioke@yahoo.com>
2023-07-18 08:05:30 +0100

    Build in the pre-build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,30 +1,3 @@
 #!/usr/bin/env bash
 
-ls -lSAR /Users/runner/Library/Developer/Xcode/DerivedData/AirbnbClone-gbbroiutnhvueyfpqbsbqhpycdzy/Build/Intermediates.noindex/ArchiveIntermediates/AirbnbClone/BuildProductsPath
-
-ls -lSAR /Users/runner/Library/Developer/Xcode/DerivedData/AirbnbClone-gbbroiutnhvueyfpqbsbqhpycdzy/Build/Intermediates.noindex/ArchiveIntermediates/AirbnbClone
-
-ls -lSAR /Users/runner/Library/Developer/Xcode/DerivedData/AirbnbClone-gbbroiutnhvueyfpqbsbqhpycdzy/Build/Intermediates.noindex/ArchiveIntermediates
-
-ls -lSAR /Users/runner/Library/Developer/Xcode/DerivedData/AirbnbClone-gbbroiutnhvueyfpqbsbqhpycdzy/Build/Intermediates.noindex
-
-ls -lSAR /Users/runner/Library/Developer/Xcode/DerivedData/AirbnbClone-gbbroiutnhvueyfpqbsbqhpycdzy/Build
-
-ls -lSAR /Users/runner/Library/Developer/Xcode/DerivedData/AirbnbClone-gbbroiutnhvueyfpqbsbqhpycdzy
-
-ls -lSAR /Users/runner/Library/Developer/Xcode/DerivedData
-
-ls -lSAR /Users/runner/Library/Developer/Xcode/DerivedData/AirbnbClone-gbbroiutnhvueyfpqbsbqhpycdzy/Build/Intermediates.noindex/ArchiveIntermediates/AirbnbClone/IntermediateBuildFilesPath/AirbnbClone.build/Release-iphoneos/AirbnbClone.build/DerivedSources
-
-echo $CONFIGURATION_BUILD_DIR
-
-cd $APPCENTER_SOURCE_DIRECTORY
-
-# echo "\n--------------------------------------------\nMain JSBundle"
-# cat /Users/runner/Library/Developer/Xcode/DerivedData/AirbnbClone-gbbroiutnhvueyfpqbsbqhpycdzy/Build/Intermediates.noindex/ArchiveIntermediates/AirbnbClone/BuildProductsPath/Release-iphoneos/main.jsbundle
-
-# echo "\n--------------------------------------------\nAirbnbClone.app:"
-# cat /Users/runner/Library/Developer/Xcode/DerivedData/AirbnbClone-gbbroiutnhvueyfpqbsbqhpycdzy/Build/Intermediates.noindex/ArchiveIntermediates/AirbnbClone/BuildProductsPath/Release-iphoneos/AirbnbClone.app
-
-# echo "\n--------------------------------------------\nmain.jsbundle.map:"
-# cat /Users/runner/Library/Developer/Xcode/DerivedData/AirbnbClone-gbbroiutnhvueyfpqbsbqhpycdzy/Build/Intermediates.noindex/ArchiveIntermediates/AirbnbClone/IntermediateBuildFilesPath/AirbnbClone.build/Release-iphoneos/AirbnbClone.build/DerivedSources/main.jsbundle.map
+yarn bundle
