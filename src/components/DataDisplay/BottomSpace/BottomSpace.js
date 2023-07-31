d7806f2034ca67381977aa880664b57d0f3039ca
Ebenezer <Ebenezer>
2023-07-30 22:06:47 -0700

    Updated to add demand form on homescreen

--- src/components/DataDisplay/BottomSpace/BottomSpace.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {View} from 'react-native';
-import {styles} from './styles';
-
-const BottomSpace = () => {
-  return <View style={styles.bottomSpace}></View>;
-};
-
-export default BottomSpace;
--- src/components/DataDisplay/ListItemText/styles.js
+++
@@ -1,4 +0,0 @@
-import {StyleSheet} from 'react-native';
-import {offsets} from '../../../assets/styles/global';
-
-export const styles = StyleSheet.create({});
