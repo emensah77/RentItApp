4bd510629a5ecedc164d3764fe69b6c33bf1c392
Chijioke Nna <nnachijioke@yahoo.com>
2023-08-01 09:57:46 +0100

    Patch PR

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,4 +3,3 @@ export {default as promiseAll} from './promiseAll';
 export {default as randomInt} from './randomInt';
 export {default as localities, getSubLocalities} from './localities';
 export {default as TYPES} from './data-types';
-export {default as Config} from './config';
