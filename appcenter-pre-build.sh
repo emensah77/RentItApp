fb9eb75bbf97701af5d3ba0b0a1ffd7f11845b79
Chijioke Nna <nnachijioke@yahoo.com>
2023-08-24 16:59:38 +0100

    Build fix

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-yarn bundle
+if [[ $PLATFORM = "ios" ]]
+then
+  yarn bundle:macos
+else
+  yarn bundle:win32
+fi
 
 ls android/app/src/main/assets
 cat android/app/src/main/assets/index.android.bundle
