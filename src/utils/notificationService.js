22d16b4cbf716c0593003bd751d6cff9c488af0b
Ebenezer <Ebenezer>
2024-01-06 20:19:09 -0800

    Updated

--- src/utils/notificationService.js
+++ src/utils/notificationService.js
@@ -12,21 +12,19 @@ async function requestUserPermission() {
 
   if (enabled) {
     console.debug('Authorization Status:', authStatus);
-    const userId = auth()?.currentUser?.uid;
-    getFcmToken(userId);
+    getFcmToken();
   }
 }
 
 export const storeFcmTokenInFirestore = async (userId, fcmToken) => {
   try {
-    // Store the FCM token in Firestore
     await firestore().collection('deviceFcms').doc(userId).set(
       {
         deviceToken: fcmToken,
         userId,
       },
       {merge: true},
-    ); // Use merge to avoid overwriting other fields
+    );
     await firestore().collection('users').doc(userId).set(
       {
         fcmToken,
@@ -40,41 +38,36 @@ export const storeFcmTokenInFirestore = async (userId, fcmToken) => {
   }
 };
 
-export const getFcmToken = async userId => {
+export const getFcmToken = async () => {
+  // Check if the user is signed in
+  const user = auth().currentUser;
+  if (!user) {
+    return;
+  }
+
+  const userId = user.uid;
   const fcmToken = await AsyncStorage.getItem('fcmToken');
 
   if (!fcmToken) {
     try {
-      // Attempt to retrieve the new FCM token
       const newFcmToken = await messaging().getToken();
       if (newFcmToken) {
-        // Store the new token in AsyncStorage
         await AsyncStorage.setItem('fcmToken', newFcmToken);
         console.debug('New FCM Token:', newFcmToken);
-
-        // Store the new token in Firestore
         await storeFcmTokenInFirestore(userId, newFcmToken);
       }
     } catch (error) {
       console.error('Error getting FCM token:', error);
     }
-  } else if (fcmToken && userId) {
-    // Existing token in AsyncStorage and the user is logged in
-    console.debug('Existing FCM Token:', fcmToken);
-
-    await storeFcmTokenInFirestore(userId, fcmToken); // Update Firestore with the existing token
   } else {
-    console.error('FCM token retrieval failed or user is not logged in');
+    console.debug('Existing FCM Token:', fcmToken);
+    await storeFcmTokenInFirestore(userId, fcmToken);
   }
 };
+
 export const notificationListener = async () => {
   messaging().onNotificationOpenedApp(remoteMessage => {
-    // console.debug(
-    //   'Notification caused app to open from background state:',
-    //   remoteMessage,
-    // );
     if (remoteMessage.data && remoteMessage.data.screen === 'Marketer Home') {
-      // Navigate to the MarketerHome screen with the notification data
       const itemDetails = JSON.parse(remoteMessage.data.itemDetails);
       navigate('MarketerDashboard', {
         screen: 'Marketer Home',
@@ -88,28 +81,23 @@ export const notificationListener = async () => {
     const itemDetails = JSON.parse(remoteMessage.data.itemDetails);
 
     if (remoteMessage.data && remoteMessage.data.screen === 'Marketer Home') {
-      // Navigate to the MarketerHome screen with the notification data
       navigate('MarketerDashboard', {
         screen: 'Marketer Home',
         params: {itemDetails},
       });
     }
-    // console.debug('received in foreground', remoteMessage),
   });
 
   messaging()
     .getInitialNotification()
     .then(remoteMessage => {
-      const itemDetails = JSON.parse(remoteMessage.data.itemDetails);
-
       if (remoteMessage && remoteMessage.data && remoteMessage.data.screen === 'Marketer Home') {
-        // Navigate to the MarketerHome screen with the notification data
+        const itemDetails = JSON.parse(remoteMessage.data.itemDetails);
         navigate('MarketerDashboard', {
           screen: 'Marketer Home',
           params: {itemDetails},
         });
       }
-
       navigate('Post', remoteMessage.data);
     });
 };
